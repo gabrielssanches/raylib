ab11aa6c5af01bc492c7694cd957d92695a24835
Gabriel dos Santos Sanches <gabrielssanches@gmail.com>
2024-03-12 08:16:05 +0100

    rcore_wii.c: wip

--- src/rcore.c
+++ src/rcore.c
@@ -502,6 +502,8 @@ const char *TextFormat(const char *text, ...);              // Formatting of tex
     #include "platforms/rcore_drm.c"
 #elif defined(PLATFORM_ANDROID)
     #include "platforms/rcore_android.c"
+#elif defined(PLATFORM_WII)
+    #include "platforms/rcore_wii.c"
 #else
     // TODO: Include your custom platform backend!
     // i.e software rendering backend or console backend!
